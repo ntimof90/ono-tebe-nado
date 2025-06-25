99ec357b169c7160d0b5d07279aca03f41fad2fd
NTimofeev <nt-90@yandex.ru>
2025-06-25 20:29:36 +0300

    feat: сделать верстку

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	// semi: false,
+	trailingComma: 'all',
+	singleQuote: true,
+	// jsxSingleQuote: true,
+	printWidth: 80,
+	useTabs: true,
+	tabWidth: 2,
+	endOfLine: 'lf',
+};
